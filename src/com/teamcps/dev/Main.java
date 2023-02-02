769d5e34711dc7bec2fdfdbc90cb699607e95f19
jpschew <jps.chew@gmail.com>
2023-02-02 19:17:49 +0800

    Initial Commit

---
+++ src/com/teamcps/dev/Main.java
@@ -0,0 +1,12 @@
+package com.teamcps.dev;
+
+import com.teamcps.dev.calculator.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1,2));
+    }
+}
